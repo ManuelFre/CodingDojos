0f95509fc3fefb3bd2c375c3fbc9f669c098352e
Manuel Freischlager <wi16b048@technikum-wien.at>
2017-10-11 19:09:15 +0200

    zwischenSave

---
+++ Inhouse_GUI/Inhouse_GUI/GeoObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inhouse_GUI
+{
+    class GeoObject
+    {
+        
+        public String Shape { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public GeoObject(string shape, int posX, int posY, int width, int height)
+        {
+            Shape = shape;
+            PosX = posX;
+            PosY = posY;
+            Width = width;
+            Height = height;
+        }
+
+
+
+    }
+}
